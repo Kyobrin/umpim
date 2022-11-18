3ac46dcf9be9cf8e7974e083c700b3e615a9e695
kyobrin <zullisun@gmail.com>
2022-11-18 20:10:36 -0300

    umpim

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿{
+Console.Clear();    
+Console.WriteLine("Um, dois tres pim!!!");    
+    {
+    for (int n = 1; n <= 40; n++)
+    if (n % 4 ==0) 
+    {
+        Console.Write("pim\n");
+
+    }
+    else
+    {
+        Console.Write($" {n,2} ");
+
+
+    }
+
+    }
+}   
